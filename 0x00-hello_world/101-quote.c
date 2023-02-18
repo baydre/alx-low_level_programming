0e9423edfef36f5eac8350b7e88f55a043ebcf13
Yasir Musa <baydreafrica@gmail.com>
2023-02-18 03:23:58 -0800

    Advance Task: Intel & Quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,14 @@
-#include <std.h>
+#include <stdio.h>
 #include <unistd.h>
+
 /**
- * main -Entry point
- *
+ * main - Entry point
  * Return: Always 1 (Failure)
+ *
  */
+
 int main(void)
 {
-	write(2, "and that place is useful\" - Dora Korpar, @015-10-19\n", 59);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
