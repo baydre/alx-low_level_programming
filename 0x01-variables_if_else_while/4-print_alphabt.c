bccd7ecbef660b19eef561f291ceecaf597e020d
Yasir Musa <baydreafrica@gmail.com>
2023-02-19 01:06:25 -0800

    Program: Prints Alphabet with the exception of 'e' & 'q'

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Print alphabet in lower-case with the exception of q & e
+ * 
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char alpha;
+
+	for (alpha = 'a'; alpha <= 'z'; alpha++)
+	{
+		if (alpha != 'q' && alpha != 'e')
+			putchar(alpha);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
