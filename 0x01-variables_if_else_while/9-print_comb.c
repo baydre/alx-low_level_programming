1a1b7c2ae8081ce3080a8a7228aafa572cf33085
Yasir Musa <baydreafrica@gmail.com>
2023-02-19 17:37:24 -0800

    Program: Print all single digit values seperated with commas

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - Prints all possible combinations of single-digit numbers
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char digit = '0';
+
+	while (digit <= '9')
+	{
+		putchar(digit);
+		if (digit != '9')
+		{
+			putchar(',');
+			putchar(' ');
+		}
+		digit++;
+	}
+	putchar('\n');
+
+	return (0);
+}
