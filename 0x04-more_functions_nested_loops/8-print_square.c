56312ff437ebc0a7b29e857b46de7baf6099be27
Yasir Musa <baydreafrica@gmail.com>
2023-02-24 07:55:51 -0800

    Program: function that prints a square

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -7,20 +7,18 @@
  */
 void print_square(int size)
 {
-	int row, column;
+	int x, y;
 
-	if (size > 0)
-	{
-		for (row = 0; row < size; row++)
-		{
-			for (column = 0; column < size; column++)
-			{
-				_putchar(35);
-			}
-			_putchar('\n');
-		}
-	}
-	else
+	if (size <= 0)
 		_putchar('\n');
+
+	for (x = 0; x < size; x++)
+	{
+		for (y = 0; y < (size); y++)
+		{
+			_putchar('#');
+		}
+		_putchar('\n');
+	}
 
 }
