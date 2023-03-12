410daf03e95d8d31d9e8ef880ac3ca4c3256d9c8
Yasir Musa <baydreafrica@gmail.com>
2023-03-11 22:35:07 -0800

    C functions files used to create libmy.a static library

---
+++ 0x09-static_libraries/1-strncat.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _strncat - Concatenates two strings using at most
+ *            an inputted number of bytes from src.
+ * @dest: The string to be appended upon.
+ * @src: The string to be appended to dest.
+ * @n: The number of bytes from src to be appended to dest.
+ *
+ * Return: A pointer to the resulting string dest.
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int x = 0, y = 0;
+
+	while (dest[x++])
+		y++;
+	for (x = 0; src[x] && x < n; x++)
+		dest[y++] = src[x];
+	return (dest);
+}
