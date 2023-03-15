f683ecbc2080c95653f81ee29cf4a62e59db340d
Yasir Musa <baydreafrica@gmail.com>
2023-03-15 13:32:13 -0700

    Added README & main.h files

---
+++ 0x0B-malloc_free/main.h
@@ -0,0 +1,14 @@
+/*
+ * File: main.h
+ * Auth: Baydre
+ * Desc: Header file containing all prototype functions
+ *
+ */
+
+int _putchar(char c);
+char *create_array(unsigned int size, char c);
+char *_strdup(char *str);
+char *str_concat(char *s1, char *s2);
+int **alloc_grid(int width, int height);
+void free_grid(int **grid, int height);
+
