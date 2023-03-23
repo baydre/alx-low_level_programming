6e1650a9e5188d80a24bb6062b38624de3d0b37f
Yasir Musa <baydreafrica@gmail.com>
2023-03-22 20:30:50 -0700

    Program: function that prints a name

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -2,7 +2,7 @@
 #include "function_pointers.h"
 
 /**
- * print_name - a function that prints a name.
+ * print_name - function that prints a name.
  * @name: pointer to name to print
  * @f: function to print name
  * Return: Nothing
