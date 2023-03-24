c31b6004122d3795dac20ce311aa5c47e9333f79
Yasir Musa <baydreafrica@gmail.com>
2023-03-24 10:37:42 -0700

    Updated header file

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,8 +1,11 @@
+#ifndef VARIADIC_FUNCTIONS_H
+#define VARIADIC_FUNCTIONS_H
+
 /*
- * File: main.h
+ * File: variadic_functions.h
  * Auth: Baydre
- * Desc: Header file containing all prototype functions
- *
+ * Desc: Header file containing prototypes for all functions
+ *       used in the 0x0F-variadic_functions directory.
  */
 
 #include <stdarg.h>
@@ -24,3 +27,5 @@ int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
+
+#endif
