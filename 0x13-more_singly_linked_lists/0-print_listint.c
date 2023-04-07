ba2a84143dd8aa136dd6787dc9cb81b5d37ba91d
Yasir Musa <baydreafrica@gmail.com>
2023-04-07 04:04:46 -0700

    Program tat prints all the elements of a listint_t list

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+ * print_listint - prints all elements of list_int list
+ * @h: pointer to struct list_int
+ * Return: number of nodes
+ */
+
+size_t print_listint(const listint_t *h)
+{
+	size_t node = 0;
+
+	while (h)
+	{
+		printf("%i\n", h->n);
+		node++;
+		h = h->next;
+	}
+
+	return (node);
+}
