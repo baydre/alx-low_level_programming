65112222330b8b6e8c4cc28dc0233368283dc528
Yasir Musa <baydreafrica@gmail.com>
2023-04-07 04:18:56 -0700

    Updated header file

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -1,3 +1,6 @@
+#ifndef _LISTS_H
+#define _LISTS_H
+
 /*
  * File: list.h
  * Auth: Baydre
@@ -33,3 +36,4 @@ int sum_listint(listint_t *head);
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n);
 int delete_nodeint_at_index(listint_t **head, unsigned int index);
 
+#endif
