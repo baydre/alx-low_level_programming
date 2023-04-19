5499352e22d9147318f1f7750f0b1e0c582c811e
Yasir Musa <baydreafrica@gmail.com>
2023-04-19 01:09:51 -0700

    Program that creates a file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -3,7 +3,7 @@
 /**
  * create_file - creates a file.
  * @filename: pointer to the name of the file to create.
- * @text_content: pointer to a string to write to the file.
+ * @text_content: points: to a string to write to the file.
  *
  * Return: 1 on success, -1 on failure.
  */
@@ -26,7 +26,7 @@ int create_file(const char *filename, char *text_content)
 	f_opn = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 	f_wrt = write(f_opn, text_content, count);
 
-	if(f_opn == -1 || f_wrt == -1)
+	if (f_opn == -1 || f_wrt == -1)
 	{
 		return (-1);
 	}
