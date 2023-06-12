d1f792a84aaa3653e8fad641d5fdf7a3392855dd
Yasir Musa <baydreafrica@gmail.com>
2023-06-12 00:47:00 -0700

    Program that prints all arguments it receives

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -1,6 +1,6 @@
 /*
- * Command line arguments, Task 2: Program that prints all arguments it receives.
- * All args should be printed, including the first one.
+ * Command line arguments, Task 2: Program that prints all arguments
+ * it receives. All args should be printed, including the first one.
  * Only print one argument per line, ending with a '\n'.
  */
 #include "main.h"
@@ -19,8 +19,7 @@ int main(int argc, char *argv[])
 	do {
 		printf("%s\n", argv[i]);
 		i++;
-	}
-	while (i < argc);
+	} while (i < argc);
 
 	return (0);
 }
