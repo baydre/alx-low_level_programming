193e8ea2332d3890691fb488b10f3d8ad36c5551
Yasir Musa <baydreafrica@gmail.com>
2023-06-19 01:50:41 -0700

    function-like macro that computes the sum of x & y

---
+++ 0x0D-preprocessor/4-sum.h
@@ -0,0 +1,12 @@
+#ifndef SUM_H
+#define SUM_H
+/*
+ * preprocessor, Task 4: function-like macr SUM(x, y) that computes
+ * the sum of the numbers x and y.
+ */
+
+#include <stdlib.h>
+
+#define SUM(X, Y) ((X) + (Y))
+
+#endif
