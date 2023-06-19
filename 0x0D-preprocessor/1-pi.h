2cd2577ca7cd3aed025837548f205847036c3234
Yasir Musa <baydreafrica@gmail.com>
2023-06-19 01:16:06 -0700

    header file that defines a macro named PI

---
+++ 0x0D-preprocessor/1-pi.h
@@ -0,0 +1,10 @@
+/*
+ * preprocessor, Task 1: header file that defines a macro named PI
+ * as an abbreviation for the token 3.14159265359.
+ */
+#ifndef MAIN_H
+#define MAIN_H
+
+#define PI 3.14159265359
+
+#endif
