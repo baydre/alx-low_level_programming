f9f3b4adecb15f45bc8a45d02950897dd72f4d0b
Yasir Musa <baydreafrica@gmail.com>
2023-06-19 01:49:50 -0700

    function-like macro that computes the absolute of x

---
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -0,0 +1,12 @@
+#ifndef FUNC_LIKE_MACRO_H
+#define FUNC_LIKE_MACRO_H
+/*
+ * preprocessor, Task 3: function-like macro ABS(x) that computes
+ * the absolute value of a number x.
+ */
+
+#include <stdlib.h>
+
+#define ABS(x) (abs(x))
+
+#endif
