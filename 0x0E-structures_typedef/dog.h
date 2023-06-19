02dea6f583f5d1dabda6d419fba6c656d74af957
Yasir Musa <baydreafrica@gmail.com>
2023-06-19 03:24:01 -0700

    header file that defines a new type struct dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,33 @@
+#ifndef DOG_H
+#define DOG_H
+/*
+ * structures_typedef, Task 0: define a new type struct dog with the
+ * following elements.
+ */
+
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * struct dog - struct type
+ * @name: dog's name
+ * @age: dog's age
+ * @owner: dog's owner.
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+/**
+ * dog_t: new struct type
+ */
+typedef struct dog dog_t;
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+
+#endif
