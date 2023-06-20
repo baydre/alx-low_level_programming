5c0cbe45d45530ace82f75d5f83a38a2566605d6
Yasir Musa <baydreafrica@gmail.com>
2023-06-20 02:34:51 -0700

    Program that prints a struct dog

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -9,20 +9,6 @@
  */
 void print_dog(struct dog *d)
 {
-	/*
-	 * if (d == NULL)
-	{
-		return;
-	}
-	if (d->name == NULL)
-	{
-		d->name = "(nil)";
-	}
-	if (d->owner == NULL)
-	{
-		d->owner = "(nil)";
-	}
-	*/
 	if (d == NULL)
 		return;
 	d->name = (d->name == NULL) ? "(nil)" : d->name;
