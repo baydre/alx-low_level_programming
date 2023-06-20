a2a6cc918ab19bb77e1960513fd1d32459fcdd7e
Yasir Musa <baydreafrica@gmail.com>
2023-06-19 19:41:48 -0700

    Program that prints a struct dog

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,18 @@
+/*
+ * structures_typedef, Task 2: function that prints a "struct dog".
+ */
+#include "dog.h"
+
+/**
+ * print_dog - prints struct dog
+ * @d: initialises the struct type dog
+ */
+void print_dog(struct dog *d)
+{
+	if (d != NULL)
+	{
+		printf("Name: %s\n", (d->name != NULL) ? d->name : "(nil)");
+		printf("Age: %.2f\n", d->age);
+		printf("Owner: %s\n", (d->name != NULL) ? d->owner : "(nil)");
+	}
+}
