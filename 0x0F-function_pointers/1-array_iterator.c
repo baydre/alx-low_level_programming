925bd2ef36ad75d55dbc3e59d4c5cadf43ba93d2
Yasir Musa <baydreafrica@gmail.com>
2023-06-21 10:24:30 -0700

    Program that executes a function given

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,22 @@
+/*
+ * function_pointers, Task 1: function that executes a function given as
+ * a parameter on each element of an array.
+ */
+#include "function_pointers.h"
+
+/**
+ * array_iterator - iterates through each element of array
+ * and calls fuction pointed to action.
+ * @array: integer array pointer.
+ * @size: integer size
+ * @action: function pointer.
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	unsigned int i;
+
+	for (i = 0; i < size; i++)
+	{
+		action(array[i]);
+	}
+}
