2e0a0c72bd1580542a7ffacbc7ffa66b17a6ec70
Yasir Musa <baydreafrica@gmail.com>
2023-06-21 10:09:37 -0700

    Program that prints a name

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,14 @@
+/*
+ * function_pointers, Task 0: function that prints a name.
+ */
+#include "function_pointers.h"
+
+/**
+ * print_name - takes two parameters 'f' and 'name'.
+ * name: the argument passed.
+ * @f: call the function.
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
