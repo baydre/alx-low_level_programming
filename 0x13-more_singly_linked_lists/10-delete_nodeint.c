0e6431dfddd3fc2dbc1cbbda55469a0a06fc6916
Yasir Musa <baydreafrica@gmail.com>
2023-07-05 15:32:35 -0700

    function that deletes the node at index of a list

---
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -0,0 +1,46 @@
+/*
+ * more_singly_linked_list, Task 10: function that deletes
+ * the node at index of list.
+ */
+#include "lists.h"
+
+/**
+ * delete_nodeint_at_index - deletes the malloc and frees
+ * the node.
+ * @head: head of the list.
+ * @index: input integer value.
+ * Return: 1 successful otherwise -1.
+ */
+int delete_nodeint_at_index(listint_t **head, unsigned int index)
+{
+	unsigned int count = 0;
+	listint_t *current = *head;
+        listint_t *prev = NULL;
+
+	if (head == NULL || *head == NULL)
+	{
+		return (-1);
+	}
+
+	if (index == 0)
+	{
+		*head = (*head)->next;
+		free(current);
+		return (1);
+	}
+
+ 	while (current != NULL)
+	{
+		if (count == index)
+		{
+			prev->next = current->next;
+			free(current);
+			return (1);
+		}
+		count++;
+		prev = current;
+		current = current->next;
+	}
+
+	return (-1);
+}
